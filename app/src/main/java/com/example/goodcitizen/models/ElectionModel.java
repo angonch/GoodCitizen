e47d72c263dd98fd5ecdaf7681cdf562fbb2c4f1
Anna Goncharenko <agoncharenko@fb.com>
2020-07-13 14:07:06 -0700

    election model: basic variables and functions to get data about elections from json response from api

---
+++ app/src/main/java/com/example/goodcitizen/models/ElectionModel.java
@@ -0,0 +1,31 @@
+package com.example.goodcitizen.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElectionModel {
+
+    private String electionName;
+    private String date;
+    private String divisionId;
+
+    public static ElectionModel fromJson(JSONObject jsonObject) throws JSONException {
+        ElectionModel election = new ElectionModel();
+        election.electionName = jsonObject.getString("name");
+        election.date = jsonObject.getString("electionDay");
+        election.divisionId = jsonObject.getString("ocdDivisionId");
+        return election;
+    }
+
+    public static List<ElectionModel> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<ElectionModel> elections = new ArrayList<>();
+        for(int i = 0; i < jsonArray.length(); i++) {
+            elections.add(fromJson(jsonArray.getJSONObject(i)));
+        }
+        return elections;
+    }
+}
---
+++ app/src/main/java/com/example/goodcitizen/models/LocationModel.java
@@ -0,0 +1,4 @@
+package com.example.goodcitizen.models;
+
+public class LocationModel {
+}
