e4729805ad31899f7db0e4e6ccb862ef644e1502
Anna Goncharenko <agoncharenko@fb.com>
2020-07-14 16:14:09 -0700

    parse sign up setup: creates and signs in a new parse user from the sign up page

--- app/src/main/java/com/example/goodcitizen/activities/SignUpActivity.java
+++ app/src/main/java/com/example/goodcitizen/activities/SignUpActivity.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.goodcitizen.R;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
 
     public static final String TAG = "SignUpActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private EditText etEmail;
     private Button btnSignUp;
 
     @Override
@@ -20,39 +28,42 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
         btnSignUp = findViewById(R.id.btnSignUp);
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.i(TAG, "onClick createAccount button");
-//                String email = etEmail.getText().toString();
-//                String username = etUsername.getText().toString();
-//                String password = etPassword.getText().toString();
-//                signUpUser(email, username, password);
-                signUpUser("", "", "");
+                String email = etEmail.getText().toString();
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signUpUser(email, username, password);
             }
         });
     }
 
     private void signUpUser(String email, String username, String password) {
         // Create the ParseUser
-//        ParseUser user = new ParseUser();
-//        // Set core properties
-//        user.setUsername(username);
-//        user.setPassword(password);
-//        user.setEmail(email);
-//        // Invoke signUpInBackground
-//        user.signUpInBackground(new SignUpCallback() {
-//            public void done(ParseException e) {
-//                if (e != null) {
-//                    Log.e(TAG, "Issue with login ", e);
-//                    Toast.makeText(SignUpActivity.this, "Issue with login!", Toast.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                goMainActivity();
-//            }
-//        });
-        goMainActivity();
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with login ", e);
+                    Toast.makeText(SignUpActivity.this, "Issue with login!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    goMainActivity();
+                }
+            }
+        });
     }
 
     private void goMainActivity() {
