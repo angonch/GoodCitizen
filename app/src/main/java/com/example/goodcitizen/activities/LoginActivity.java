ac2700d790bd9a2772b41f2b9d5f6173847f5834
Anna Goncharenko <agoncharenko@fb.com>
2020-07-14 10:23:54 -0700

    login and sign up skeleton: default page set to login page, navigation to main activity

--- app/src/main/java/com/example/goodcitizen/activities/LoginActivity.java
+++ app/src/main/java/com/example/goodcitizen/activities/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.goodcitizen.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +12,63 @@ import com.example.goodcitizen.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static final String TAG = "LoginActivity";
+    private Button btnLogin;
+    private Button btnSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick login button");
+//                String username = etUsername.getText().toString();
+//                String password = etPassword.getText().toString();
+//                loginUser(username, password);
+                loginUser("", "");
+            }
+        });
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goSignUpActivity();
+            }
+        });
     }
+
+    private void goSignUpActivity() {
+        Intent i = new Intent(this, SignUpActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user " + username);
+        // navigate to the main activity if user has signed in properly
+//        ParseUser.logInInBackground(username, password, new LogInCallback() {
+//            @Override
+//            public void done(ParseUser user, ParseException e) {
+//                if (e != null) {
+//                    // TODO: better error handling
+//                    Log.e(TAG, "Issue with login ", e);
+//                    Toast.makeText(LoginActivity.this, "Issue with login!", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                // go to main activity
+//                goMainActivity();
+//            }
+//        });
+        goMainActivity();
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/goodcitizen/activities/SignUpActivity.java
+++ app/src/main/java/com/example/goodcitizen/activities/SignUpActivity.java
@@ -1,6 +1,10 @@
 package com.example.goodcitizen.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +12,52 @@ import com.example.goodcitizen.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    public static final String TAG = "SignUpActivity";
+    private Button btnSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick createAccount button");
+//                String email = etEmail.getText().toString();
+//                String username = etUsername.getText().toString();
+//                String password = etPassword.getText().toString();
+//                signUpUser(email, username, password);
+                signUpUser("", "", "");
+            }
+        });
+    }
+
+    private void signUpUser(String email, String username, String password) {
+        // Create the ParseUser
+//        ParseUser user = new ParseUser();
+//        // Set core properties
+//        user.setUsername(username);
+//        user.setPassword(password);
+//        user.setEmail(email);
+//        // Invoke signUpInBackground
+//        user.signUpInBackground(new SignUpCallback() {
+//            public void done(ParseException e) {
+//                if (e != null) {
+//                    Log.e(TAG, "Issue with login ", e);
+//                    Toast.makeText(SignUpActivity.this, "Issue with login!", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                goMainActivity();
+//            }
+//        });
+        goMainActivity();
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
