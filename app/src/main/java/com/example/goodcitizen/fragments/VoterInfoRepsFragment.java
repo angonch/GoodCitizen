744c2f261f4aa87b4dca0234a7117863bbf0e3ea
Anna Goncharenko <agoncharenko@fb.com>
2020-07-14 10:14:41 -0700

    voter info tabs: switch between two tabs on the voter info page, one for jurisdiction info and another for representatives, default is set to jurisdiction tab

--- app/src/main/java/com/example/goodcitizen/fragments/VoterInfoRepsFragment.java
+++ app/src/main/java/com/example/goodcitizen/fragments/VoterInfoRepsFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.goodcitizen.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class VoterInfoRepsFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private FragmentManager fragmentManager;
 
     // The onCreateView method is called when Fragment should create its View object hierarchy,
     // either dynamically or via XML layout inflation.
@@ -25,5 +31,33 @@ public class VoterInfoRepsFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        super.onViewCreated(view, savedInstanceState);
+        fragmentManager = getChildFragmentManager();
+        tabLayout = view.findViewById(R.id.tlVoterInfo);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            private boolean alreadyReselected = false;
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment fragment;
+                if(tab.getPosition() == 0) {
+                    fragment = new JurisdictionFragment();
+                    Toast.makeText(getContext(), "Jurisdiction info page!", Toast.LENGTH_SHORT).show();
+                } else {
+                    fragment = new RepresentativesFragment();
+                    Toast.makeText(getContext(), "Representatives page!", Toast.LENGTH_SHORT).show();
+                }
+                fragmentManager.beginTransaction().replace(R.id.flVoterInfoContainer, fragment).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+        // sets default selected tab to be the first one (need to select one and switch back to tab needed)
+        tabLayout.getTabAt(1).select();
+        tabLayout.getTabAt(0).select();
     }
 }
