c5e8b5e6219ae865430cf6feeba3df80f7273e04
Anna Goncharenko <agoncharenko@fb.com>
2020-07-14 08:25:12 -0700

    bottom navigation: added bottom navigation to main activity to switch between the three main fragments

--- app/src/main/java/com/example/goodcitizen/activities/MainActivity.java
+++ app/src/main/java/com/example/goodcitizen/activities/MainActivity.java
@@ -1,16 +1,58 @@
 package com.example.goodcitizen.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.goodcitizen.R;
+import com.example.goodcitizen.fragments.ElectionsFragment;
+import com.example.goodcitizen.fragments.LocationsFragment;
+import com.example.goodcitizen.fragments.VoterInfoRepsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                // menu item is one of the three items - check which item has been tapped
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_elections:
+                        fragment = new ElectionsFragment();
+                        Toast.makeText(getApplicationContext(), "Elections page!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_locations:
+                        fragment = new LocationsFragment();
+                        Toast.makeText(getApplicationContext(), "Locations page!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_reps:
+                    default:
+                        fragment = new VoterInfoRepsFragment();
+                        Toast.makeText(getApplicationContext(), "Voter Info page!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                // swap out fragments
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_elections);
     }
 }
