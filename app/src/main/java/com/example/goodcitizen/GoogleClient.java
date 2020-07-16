a7986824b5dac31a24a5b7958c9ab4421f974e0a
Anna Goncharenko <agoncharenko@fb.com>
2020-07-16 09:14:11 -0700

    connect to api: get elections query response

--- app/src/main/java/com/example/goodcitizen/GoogleClient.java
+++ app/src/main/java/com/example/goodcitizen/GoogleClient.java
@@ -1,6 +1,13 @@
 package com.example.goodcitizen;
 
+import android.content.Context;
+
 public class GoogleClient {
 
     public static final String TAG = "GoogleClient";
+    public static final String ELECTIONS_URL = "https://www.googleapis.com/civicinfo/v2/elections?key=";
+
+    public static String getElectionsUrl(Context context) {
+        return ELECTIONS_URL + context.getResources().getString(R.string.API_KEY);
+    }
 }
--- app/src/main/java/com/example/goodcitizen/fragments/ElectionsFragment.java
+++ app/src/main/java/com/example/goodcitizen/fragments/ElectionsFragment.java
@@ -1,15 +1,31 @@
 package com.example.goodcitizen.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.goodcitizen.GoogleClient;
 import com.example.goodcitizen.R;
+import com.example.goodcitizen.models.ElectionModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class ElectionsFragment extends Fragment {
+
+    public static final String TAG = "ElectionsFragment";
+    List<ElectionModel> elections;
 
     // The onCreateView method is called when Fragment should create its View object hierarchy,
     // either dynamically or via XML layout inflation.
@@ -25,5 +41,30 @@ public class ElectionsFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        super.onViewCreated(view, savedInstanceState);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(GoogleClient.getElectionsUrl(getContext()), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    //elections.addAll(ElectionModel.fromJsonArray(results)); // Modify elections list
+                    //electionAdapter.notifyDataSetChanged();
+                    //Log.i(TAG, "Elections: " + elections.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit json exception", e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure", throwable);
+            }
+        });
     }
 }
