5273510894e8225e1e8bb57e7f0ee1fa1b789e72
Anna Goncharenko <agoncharenko@fb.com>
2020-07-29 11:14:19 -0700

    representative details UI: show party color in background

--- app/src/main/java/com/example/goodcitizen/activities/RepresentativeDetailsActivity.java
+++ app/src/main/java/com/example/goodcitizen/activities/RepresentativeDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.goodcitizen.activities;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,6 +21,7 @@ public class RepresentativeDetailsActivity extends AppCompatActivity {
     TextView tvPosition;
     TextView tvParty;
     ImageView ivPhoto;
+    ImageView ivPartyBackground;
 
     RepresentativeModel representative;
 
@@ -32,6 +34,7 @@ public class RepresentativeDetailsActivity extends AppCompatActivity {
         tvPosition = findViewById(R.id.tvPosition);
         tvParty = findViewById(R.id.tvParty);
         ivPhoto = findViewById(R.id.ivPhoto);
+        ivPartyBackground = findViewById(R.id.ivPartyBackground);
 
         // unwrap representative data
         representative = Parcels.unwrap(getIntent().getParcelableExtra(RepresentativeModel.class.getSimpleName()));
@@ -40,8 +43,27 @@ public class RepresentativeDetailsActivity extends AppCompatActivity {
         tvOfficialName.setText(representative.getOfficialName());
         tvPosition.setText(representative.getPosition());
         tvParty.setText(representative.getParty());
+        // set color of image view for party background color
+        Integer color;
+        switch(representative.getParty()) {
+            case "Democratic Party":
+                color = getApplicationContext().getResources().getColor(R.color.democratic);
+                break;
+            case "Republican Party":
+                color = getApplicationContext().getResources().getColor(R.color.republican);
+                break;
+            case "Nonpartisan":
+            default:
+                color = getApplicationContext().getResources().getColor(R.color.nonpartisan);
+                break;
+        }
+        ivPartyBackground.setColorFilter(color);
+        tvParty.setTextColor(color);
         if(!representative.getPhotoUrl().isEmpty()) {
+            ivPhoto.setVisibility(View.VISIBLE);
             Glide.with(getApplicationContext()).load(representative.getPhotoUrl()).into(ivPhoto);
+        } else {
+            ivPhoto.setVisibility(View.GONE);
         }
     }
 }
