2c4588c06ca6e716c25392fc3010f6cce775f30a
Anna Goncharenko <agoncharenko@fb.com>
2020-08-04 14:57:43 -0700

    setting up onboarding fragments: blank fragments for onboarding activity

---
+++ app/src/main/java/com/example/goodcitizen/activities/OnboardingActivity.java
@@ -0,0 +1,16 @@
+package com.example.goodcitizen.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.goodcitizen.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
