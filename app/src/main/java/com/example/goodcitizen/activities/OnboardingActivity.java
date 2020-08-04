839bc50798ca0c51270a2700ac24c6e44a485203
Anna Goncharenko <agoncharenko@fb.com>
2020-08-04 15:03:46 -0700

    fragment adapter added to onboarding activity: adapter added and set to viewpager to indicate which fragment associates to each tab

--- app/src/main/java/com/example/goodcitizen/activities/OnboardingActivity.java
+++ app/src/main/java/com/example/goodcitizen/activities/OnboardingActivity.java
@@ -1,16 +1,53 @@
 package com.example.goodcitizen.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.goodcitizen.R;
+import com.example.goodcitizen.fragments.OnboardingFragment1;
+import com.example.goodcitizen.fragments.OnboardingFragment2;
+import com.example.goodcitizen.fragments.OnboardingFragment3;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
 
-public class OnboardingActivity extends AppCompatActivity {
+public class OnboardingActivity extends FragmentActivity {
+
+    private ViewPager pager;
+    private SmartTabLayout indicator;
+    private Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding);
+
+        pager = findViewById(R.id.vpPager);
+        indicator = findViewById(R.id.stlIndicator);
+        next = findViewById(R.id.btnNext);
+
+        FragmentStatePagerAdapter adapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return new OnboardingFragment1();
+                    case 1:
+                        return new OnboardingFragment2();
+                    case 2:
+                    default:
+                        return new OnboardingFragment3();
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        };
+        pager.setAdapter(adapter);
+        indicator.setViewPager(pager);
     }
 }
