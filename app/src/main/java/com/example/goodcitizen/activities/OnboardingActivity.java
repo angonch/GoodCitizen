342fbe42bcf670da2e3b4b2d197d20d2aac531e0
Anna Goncharenko <agoncharenko@fb.com>
2020-08-04 15:08:21 -0700

    listeners in onboarding activity and finish method: finishing onboarding activities changes preferences to not show it again

--- app/src/main/java/com/example/goodcitizen/activities/OnboardingActivity.java
+++ app/src/main/java/com/example/goodcitizen/activities/OnboardingActivity.java
@@ -1,6 +1,9 @@
 package com.example.goodcitizen.activities;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
@@ -49,5 +52,43 @@ public class OnboardingActivity extends FragmentActivity {
         };
         pager.setAdapter(adapter);
         indicator.setViewPager(pager);
+
+        // button listeners
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(pager.getCurrentItem() == 2) {
+                    finishOnboarding();
+                } else {
+                    pager.setCurrentItem(pager.getCurrentItem() + 1, true);
+                }
+            }
+        });
+
+        indicator.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 2){
+                    next.setText("Done");
+                } else {
+                    next.setText("Next");
+                }
+            }
+        });
+    }
+
+    private void finishOnboarding() {
+        // Get the shared preferences
+        SharedPreferences preferences = getSharedPreferences("my_preferences", MODE_PRIVATE);
+
+        // Set onboarding_complete to true
+        preferences.edit().putBoolean("onboarding_complete",true).apply();
+
+        // Launch the main Activity, called MainActivity
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+
+        // Close the OnboardingActivity
+        finish();
     }
 }
