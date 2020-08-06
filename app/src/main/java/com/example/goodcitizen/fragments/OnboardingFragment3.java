aa513ee9a8f72ceb5fc6800d83a4c205ce3f0d2f
Anna Goncharenko <agoncharenko@fb.com>
2020-08-06 10:36:04 -0700

    onboarding register to vote button: clicking button leads to vote.gov where user can register to vote

--- app/src/main/java/com/example/goodcitizen/fragments/OnboardingFragment3.java
+++ app/src/main/java/com/example/goodcitizen/fragments/OnboardingFragment3.java
@@ -1,20 +1,44 @@
 package com.example.goodcitizen.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.goodcitizen.R;
 
 public class OnboardingFragment3 extends Fragment {
 
+    Button btnRegister;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_onboarding3, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnRegister = view.findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://vote.gov/"));
+                startActivity(intent);
+            }
+        });
     }
 }
